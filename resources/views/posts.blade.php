a92d10ffb09f2e18b6533cb6df801839540d9f75
Rustam Davletshin <rdavletshin@live.com>
2024-03-08 14:25:43 +0100

    first commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <?php foreach ($posts as $post) : ?>
+    <article>
+        
+    </article>
+    <?php endforeach; ?>
+</body>
